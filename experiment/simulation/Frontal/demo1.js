8c567d95dc63067c189568ced85375be591ad563
Jasbir Singh <jasbirjassy6@gmail.com>
2021-11-01 12:30:39 +0530

    Bio-Medical Instrumentation

--- experiment/simulation/Frontal/demo1.js
+++ experiment/simulation/Frontal/demo1.js
@@ -540,6 +540,8 @@ if ( is_connected_1_10&&is_connected_2_11&&is_connected_3_12&&is_connected_4_13&
 
             document.getElementById("video").style.display = "block";
             document.getElementById("ACQ").style.display = "none";
+            document.getElementById("graph").disabled = false;
+
            
 
 
